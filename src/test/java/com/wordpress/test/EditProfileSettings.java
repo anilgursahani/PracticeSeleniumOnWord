d8383c103e3335a1855b4a567b6854a30d3618cc
Anil Gursahani <anilgursahani59@gmail.com>
2019-04-09 12:19:26 -0500

    First commit of file. Used to set profile to different colors

---
+++ src/test/java/com/wordpress/test/EditProfileSettings.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wordpress.test;
+
+/**
+ *
+ * @author anilg
+ */
+public class EditProfileSettings {
+   public static final String DefaultProfile = "DefaultProfile";
+   public static final String ColorLightProfile = "ColorLightProfile";
+   public static final String ColorBlueProfile = "ColorBlueProfile";
+   public static final String ColorCoffeeProfile = "ColorCoffeeProfile"  ;
+   public static final String ColorEctoplasmProfile = "ColorEctoplasmProfile" ;
+   public static final String ColorMidnightProfile = "ColorMidnightProfile" ;
+   public static final String ColorOceanProfile = "ColorOceanProfile" ;
+   public static final String ColorSunriseProfile = "ColorSunriseProfile" ;
+   
+   
+}
